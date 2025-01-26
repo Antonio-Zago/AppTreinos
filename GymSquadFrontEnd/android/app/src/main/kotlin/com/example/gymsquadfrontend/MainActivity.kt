1c1586d23a8877280661e33c1b072e84f9bb487b
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2025-01-26 09:43:03 -0300

    GS-2.7 - Ajustes na tela de login

--- GymSquadFrontEnd/android/app/src/main/kotlin/com/example/gymsquadfrontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gymsquadfrontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ gym_squad_front_end/android/app/src/main/kotlin/com/example/gym_squad_front_end/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gym_squad_front_end
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
